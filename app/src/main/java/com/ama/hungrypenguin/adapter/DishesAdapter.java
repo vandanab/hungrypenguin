5d6a1ef8ed207edcfeebc46500b24ad02d181bcc
Vandana Bachani <vbachani@vbachani-macbookpro2.roam.corp.google.com>
2016-02-21 00:57:19 -0800

    goto detail

--- app/src/main/java/com/ama/hungrypenguin/adapter/DishesAdapter.java
+++ app/src/main/java/com/ama/hungrypenguin/adapter/DishesAdapter.java
@@ -1,8 +1,8 @@
 package com.ama.hungrypenguin.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +13,8 @@ import android.widget.TextView;
 
 import com.ama.hungrypenguin.R;
 import com.ama.hungrypenguin.model.Dish;
+import com.ama.hungrypenguin.ui.FoodDetailActivity;
+import com.ama.hungrypenguin.ui.MainActivity;
 import com.ama.hungrypenguin.util.PrefsEventHandler;
 import com.ama.hungrypenguin.util.SharedPrefsHelper;
 import com.bumptech.glide.Glide;
@@ -27,10 +29,13 @@ public class DishesAdapter extends
     private List<Dish> dishes;
     private SharedPrefsHelper sharedPrefsHelper;
     private PrefsEventHandler handler;
+    private Context context;
 
-    public DishesAdapter(List<Dish> data, SharedPrefsHelper helper, PrefsEventHandler handler) {
+    public DishesAdapter(Context parent, List<Dish> data, SharedPrefsHelper helper,
+            PrefsEventHandler handler) {
         dishes = data;
         sharedPrefsHelper = helper;
+        this.context = parent;
         this.handler = handler;
     }
 
@@ -39,13 +44,14 @@ public class DishesAdapter extends
         LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(
                 Context.LAYOUT_INFLATER_SERVICE);
         View v = inflater.inflate(R.layout.dish_order_card, parent, false);
-        DishesAdapter.ViewHolder vh = new DishesAdapter.ViewHolder(v);
+        DishesAdapter.ViewHolder vh = new DishesAdapter.ViewHolder(v, context);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(DishesAdapter.ViewHolder holder, int position) {
         Dish dish = dishes.get(position);
+        holder.item = dish;
         holder.title.setText(dish.name);
         holder.cost.setText(Double.toString(dish.cost));
         holder.cost.setTag(dish.id);
@@ -55,11 +61,11 @@ public class DishesAdapter extends
         Glide.with(mContext).load(uri).asGif().into(holder.thumbnail);
 
         holder.cost.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               sharedPrefsHelper.updateOrder((Integer) v.getTag(), 1);
-               handler.showState();
-           }
+            @Override
+            public void onClick(View v) {
+                sharedPrefsHelper.updateOrder((Integer) v.getTag(), 1);
+                handler.showState();
+            }
         });
     }
 
@@ -68,18 +74,28 @@ public class DishesAdapter extends
         return dishes.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        CardView cv;
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        Dish item;
         ImageView thumbnail;
         TextView title;
         Button cost;
+        Context context;
 
-        public ViewHolder(View itemView) {
+        public ViewHolder(View itemView, Context ctx) {
             super(itemView);
-            cv = (CardView)itemView.findViewById(R.id.cv);
+            context = ctx;
+            itemView.setClickable(true);
+            itemView.setOnClickListener(this);
             thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
             title = (TextView) itemView.findViewById(R.id.title);
             cost = (Button) itemView.findViewById(R.id.costButton);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(context, FoodDetailActivity.class);
+            i.putExtra("id", item.id);
+            context.startActivity(i);
         }
     }
 }
