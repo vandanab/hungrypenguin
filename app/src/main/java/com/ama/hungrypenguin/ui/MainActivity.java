a4d8641ec544ba0ae174b4484defba126fe89a4d
Vandana Bachani <vbachani@vbachani-macbookpro2.roam.corp.google.com>
2016-02-20 18:03:08 -0800

    fixing the build error because of missing fragment imports

--- app/src/main/java/com/ama/hungrypenguin/ui/MainActivity.java
+++ app/src/main/java/com/ama/hungrypenguin/ui/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.ama.hungrypenguin.TrendingFragment;
+import com.ama.hungrypenguin.MenusFragment;
 
 import com.ama.hungrypenguin.R;
 
