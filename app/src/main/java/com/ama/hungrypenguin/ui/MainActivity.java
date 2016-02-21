94a680e9c588ccff2dbae5374099c16861796cbf
Akshay <akshay.pushparaja@gmail.com>
2016-02-20 16:39:21 -0800

    card view reference

--- app/src/main/java/com/ama/hungrypenguin/ui/MainActivity.java
+++ app/src/main/java/com/ama/hungrypenguin/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ama.hungrypenguin;
+package com.ama.hungrypenguin.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.ama.hungrypenguin.R;
 
 public class MainActivity extends AppCompatActivity {
 
