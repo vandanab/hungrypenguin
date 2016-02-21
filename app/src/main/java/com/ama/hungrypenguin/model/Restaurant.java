ac744b9b186f2c9acde34f1fd3a457afcb9f55b6
Akshay <akshay.pushparaja@gmail.com>
2016-02-20 17:14:10 -0800

    defined 2 models

---
+++ app/src/main/java/com/ama/hungrypenguin/model/Restaurant.java
@@ -0,0 +1,16 @@
+package com.ama.hungrypenguin.model;
+
+/**
+ * Created by akshay on 2/20/16.
+ */
+public class Restaurant {
+
+    String imageUrl;
+    String name;
+
+
+    public Restaurant(String imageUrl, String name){
+        this.imageUrl = imageUrl;
+        this.name = name;
+    }
+}
